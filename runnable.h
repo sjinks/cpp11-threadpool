de5216bf79af7a3bc7f00fa7e4f39def70580fb0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-02 19:29:30 +0200

    Added noexcept

--- runnable.h
+++ runnable.h
@@ -6,8 +6,8 @@ public:
 	Runnable(void) : m_ref(0) {}
 	virtual ~Runnable(void) noexcept {}
 
-	bool autoDelete(void) const { return this->m_ref != -1; }
-	void setAutoDelete(bool v) { this->m_ref = v ? 0 : -1; }
+	bool autoDelete(void) const noexcept { return this->m_ref != -1; }
+	void setAutoDelete(bool v) noexcept { this->m_ref = v ? 0 : -1; }
 
 	virtual void run(void) = 0;
 private:
